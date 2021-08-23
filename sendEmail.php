b44440c424e75deebe463b57a442fd08cc945d1c
khangduy2008 <77973360+khangduy2008@users.noreply.github.com>
2021-08-23 16:14:43 +0700

    hello

---
+++ sendEmail.php
@@ -0,0 +1,55 @@
+<?php 
+if(isset($_POST['name']) && isset($_POST['email'])){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $message = $_POST['message'];
+
+
+
+        include('./phpmailer/class.smtp.php');
+        include "./phpmailer/class.phpmailer.php"; 
+        
+
+
+        $nFrom = 'ominifood';    
+        $mFrom = 'midnightdevteam@gmail.com'; 
+        $mPass = 'friendmakemoney';       
+        $nTo = 'nguyen cuong'; 
+        $mTo = 'trankhangduy80@gmail.com';   
+        $mail             = new PHPMailer();
+        $body             = '<strong>Họ và tên:  </strong>'.$name.'<br>'.'<strong>Email:  </strong>'.$email.'<br>'.'<strong>Số điện thoại: </strong>'.$phone.'<br>'.'<strong>lời nhắn: </strong>'.$message.';
+        $title = 'feeedback website | '.$name;  
+        $mail->IsSMTP();             
+        $mail->CharSet  = "utf-8";
+        $mail->SMTPDebug  = 0;  
+        $mail->SMTPAuth   = true; 
+        $mail->SMTPSecure = "ssl"; 
+        $mail->Host       = "smtp.gmail.com";
+        $mail->Port       = 465; 
+
+        $mail->Username   = $mFrom;  
+        $mail->Password   = $mPass;    
+        $mail->SetFrom($mFrom, $nFrom);
+        $mail->AddReplyTo($email, $name); 
+        $mail->Subject    = $title;
+        $mail->MsgHTML($body);
+        $mail->AddAddress($mTo, $nTo);
+
+if ($mail->send()) {
+    $status = "success";
+    $response = "Đã gửi email";
+} else {
+    $status = "error";
+    $response = "Có lỗi: <br><br>" . $mail->ErrorInfo;
+}
+
+exit(json_encode(array("status" => $status, "response" => $response)));
+
+}
+
+
+
+
+?>
+                           
